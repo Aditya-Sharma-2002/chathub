b3758b1c64156afec88d2d40bca0607c4bade432
Aditya-Sharma-2002 <adityasharma21902@gmail.com>
2024-09-05 00:34:17 +0530

    Advancement in forgot password page

--- frontend/src/user/Forgot.jsx
+++ frontend/src/user/Forgot.jsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react";
+
 function Forgot(){
+
+    const [email,setEmail] = useState('');
+    const [otp,setOtp] = useState();
+    const [password,setPassword] = useState('');
+    const [repassword,setRepassword] = useState('');
+    const [active,setActive] = useState(false);
+
+    useEffect(() => {
+        if (email.length > 0 && otp.length > 0 && password === repassword && password.length > 0) {
+            setActive(true);
+        } else {
+            setActive(false);
+        }
+    }, [email, otp, password, repassword]);
+
+    function handleSubmit(e){
+        e.preventDefault();
+        if(email.length != 0 && otp!= 0 && password === repassword)
+            setActive(true);
+    }
+
     return(
         <div className="container">
-            <p>Forgot Password Page</p>
+            <form onSubmit={handleSubmit}>
+                <label>Email <input type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)}/></label><br/>
+                <label>OTP <input type="number" placeholder="Enter otp" onChange={(e) => setOtp(e.target.value)}/></label><br/>
+                <label>New Password <input type="password" placeholder="Enter new password" onChange={(e) => setPassword(e.target.value)}/></label><br/>
+                <label>Re-enter Password <input type="password" placeholder="Re-enter your password" onChange={(e) => setRepassword(e.target.value)}/></label><br/>
+                <button disabled={active}>Change Password</button>
+            </form>
         </div>
     );
 }
