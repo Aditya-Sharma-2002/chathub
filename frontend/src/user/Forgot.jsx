41227641068839cbd1c294129d7aa87b28d338d6
Aditya-Sharma-2002 <adityasharma21902@gmail.com>
2025-08-04 23:54:41 +0530

    Rate limiting applied

--- frontend/src/user/Forgot.jsx
+++ frontend/src/user/Forgot.jsx
@@ -28,17 +28,38 @@ function Forgot() {
 
         const response = await forgot(email);
 
-        if (response?.error) {
-            setFormErrors({ email: response.data.error });
+        // if (response?.error) {
+        //     setFormErrors({ email: response.data.error });
+        //     return;
+        // }
+
+        // if (response?.otp) {
+        //     setServerOTP(response.otp);
+        //     setStep(2);
+        // } else {
+        //     if (response?.error) {
+        //         console.log(response);
+        //         setFormErrors({ email: response.error });
+        //         return;
+        //     } else {
+        //         setFormErrors({ email: "Failed to send OTP" });
+        //     }
+        // }
+
+        
+        if (response.error) {
+            setFormErrors({ email: response.error });
             return;
         }
 
-        if (response?.otp) {
+        if (response.otp) {
             setServerOTP(response.otp);
             setStep(2);
         } else {
             setFormErrors({ email: "Failed to send OTP" });
         }
+
+        
     }
 
     function handleOTPSubmit(e) {
