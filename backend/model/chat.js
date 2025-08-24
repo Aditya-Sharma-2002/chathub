baaf701888ba5850cd75a6fff7a496f6aaf252e7
Aditya-Sharma-2002 <adityasharma21902@gmail.com>
2025-08-24 22:29:37 +0530

    Model methods created for message encryption,  incompletete

--- backend/model/chat.js
+++ backend/model/chat.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+const algorithm = 'aes-256-cbc';
+const secretKey = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
 
 const chatSchema = new mongoose.Schema({
     chatName: {
@@ -24,5 +29,33 @@ const chatSchema = new mongoose.Schema({
         ref: 'User'
     }
 }, { timestamps: true });
+
+chatSchema.methods.encryptContent = function(content) {
+    try{
+        const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+        let encrypted = cipher.update(content, "utf8", "hex");
+        encrypted += cipher.final("hex");
+        return { iv: iv.toString("hex"), content: encrypted };
+    }catch(err){
+        console.log(err);
+        return 'Some error has occured ' + err;
+    }
+}
+
+chatSchema.methods.decryptContent = function(encryptedObj){
+    try{
+        const decipher = crypto.createDecipheriv(
+            algorithm,
+            secretKey,
+            Buffer.from(encryptedObj.iv, "hex")
+        );
+        let decrypted = decipher.update(encryptedObj.content, "hex", "utf8");
+        decrypted += decipher.final("utf8");
+        return decrypted;
+    }catch(err){
+        console.log(err);
+        return 'Some error has occured ' + err ;
+    }
+}
 
 module.exports = mongoose.model('Chat', chatSchema);
